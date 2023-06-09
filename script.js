326ca19eb0b9d611b519a419a60ed067240a88e0
Sugan_T <t.suganeeshwaran@gmail.com>
2023-06-09 23:32:34 +0530

    Event Delegation and Button Scrolling

--- script.js
+++ script.js
@@ -29,3 +29,64 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////// BUTTON SCROLLING WITH SMOOTH EFFECT //////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  // OLD WAY OF SCROLLING SMOOTH EFFECT //
+
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // MODERN WAY OF SCROLLING SMOOTH EFFECT //
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('mouseenter');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+///////////////////////// EVENT Delegation /////////////////////////////
+
+// Using Event in Parent Element to perform action in its child elements
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  ///Matching Strategy //
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
